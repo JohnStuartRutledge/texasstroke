68667e592103671a9ad49b0ca15fb7a29611b518
John S. Rutledge <stu@mindwink.com>
2012-02-03 23:20:39 -0600

    first commit

---
+++ testing/revisedFormat.js
@@ -0,0 +1,135 @@
+{"type"    : "FeatureCollection","features": [
+{
+    "TSA_id"  : "A",
+    "TSA_name": "Panhandle",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "B",
+    "TSA_name": "BRAC",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "C",
+    "TSA_name": "North Texas",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "D",
+    "TSA_name": "Big Country",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "E",
+    "TSA_name": "North Central Texas",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "F",
+    "TSA_name": "Northeast Texas",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "G",
+    "TSA_name": "Piney Woods",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "H",
+    "TSA_name": "Deep East Texas",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "I",
+    "TSA_name": "Border",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "J",
+    "TSA_name": "Texas J",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "K",
+    "TSA_name": "Concho Valley",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "L",
+    "TSA_name": "Central Texas",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "M",
+    "TSA_name": "Heart of Texas",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "N",
+    "TSA_name": "Brazos Valley",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "O",
+    "TSA_name": "Capital Area Trauma",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "P",
+    "TSA_name": "Southwest Texas",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "Q",
+    "TSA_name": "Southeast Texas Trauma",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "R",
+    "TSA_name": "East Texas Gulf Coast",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "S",
+    "TSA_name": "Golden Cresent",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "T",
+    "TSA_name": "Seven Flags",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "U",
+    "TSA_name": "Coastal Bend",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+},
+{
+    "TSA_id"  : "V",
+    "TSA_name": "Lower Rio Grande Valley",
+    "geometry": {"type":"Polygon","coordinates":[]},
+    "counties": {}
+}
+
+]}
